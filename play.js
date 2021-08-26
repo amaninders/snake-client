2d04f5a6d5ccd1f1e391a3be61f5aa0d79388ed5
amaninders <amandeolofficial@gmail.com>
2021-08-26 06:30:17 +0000

    enabled the input module for our snake client

--- play.js
+++ play.js
@@ -1,3 +1,7 @@
-const { connect } = require('./client');
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
+console.log("Connecting ...");
 connect();
+
+setupInput();
