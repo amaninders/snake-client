565f17958ac179ed6f812d79ad7554697fd194b0
amaninders <amandeolofficial@gmail.com>
2021-08-26 04:59:09 +0000

    exported the connect function to its own module and refactored the code

--- play.js
+++ play.js
@@ -1,21 +1,3 @@
-const net = require('net');
+const { connect } = require('./client');
 
-// make a connection with the game server
-const connect = () => {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.setDefaultEncoding('utf-8');
-
-  conn.on('data', message => {
-    console.log(`message from server: ${message}`);
-  });
-
-  return conn;
-
-};
-
-console.log('connecting...');
 connect();
